de14aa7efd34b655783b95283866f05c3545d487
Altaly <102324558+Altaly@users.noreply.github.com>
2026-09-09 14:46:36 -0700

    style(FED-025): integrar identidad visual de Fedra

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -14,9 +14,9 @@ export default async function AppLayout({
   if (!usuario) redirect("/login");
 
   return (
-    <div className="flex min-h-screen bg-[#f8f6f2]">
+    <div className="fedra-shell flex min-h-screen bg-[#f8f6f2]">
       <Sidebar rol={usuario.rol} />
-      <div className="flex flex-1 flex-col">
+      <div className="relative z-[1] flex flex-1 flex-col">
         {/* Móvil: barra con hamburguesa + menú deslizable */}
         <MobileTopBar rol={usuario.rol} nombre={usuario.nombre} />
 
